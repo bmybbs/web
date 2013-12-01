eb4ad9fc6d36cc2e25f01b795e70c4971a929755
IronBlood <aplshenyang@gmail.com>
2013-12-01 16:30:20 +0800

    版面模板化

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 App = Ember.Application.create();
 
+var appkey = 'newweb';
+
 App.Router.map(function() {
 	// put your routes here
 	this.resource('board', { path: ':board_name' });
@@ -8,3 +10,44 @@ App.Router.map(function() {
 App.IndexRoute = Ember.Route.extend({
 
 });
+
+App.BoardRoute = Ember.Route.extend({
+	model: function(params) {
+		$.ajax({
+			type: "GET",
+			url: 'api/board/info?userid=' + localStorage.userid + '&sessid=' + localStorage.sessid + '&bname=' + params.board_name + '&appkey=' + appkey,
+			dataType: 'json',
+			success: function(data) {
+				return data;
+			}
+		});
+	}
+});
+
+Ember.Handlebars.helper('bmyBBM', function(items) {
+	// 输出大版主
+	var out="";
+	for(var i=0; i<4; i++) {
+		if(items[i] == null)
+			break;
+		out = out + " <a href='#'>" + items[i] + "</a>";
+	}
+	return out;
+});
+
+Ember.Handlebars.helper('bmySBM', function(items) {
+	// 输出小版主
+	var out="";
+	for(var i=4; i<items.length; i++) {
+		if(items[i] == null)
+			break;
+		out = out + " <a href='#'>" + items[i] + "</a>";
+	}
+	return out;
+});
+
+Ember.Handlebars.helper('bmyDate', function(tid) {
+	var date = new Date(tid*1000);
+
+	return date.toLocaleString();
+})
