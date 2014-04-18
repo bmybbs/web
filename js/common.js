cc960eb493cfb43098f4254fb73ee6de6c1f939f
IronBlood <aplshenyang@gmail.com>
2014-04-18 11:19:19 +0800

    新增获取版面列表的接口调用

--- js/common.js
+++ js/common.js
@@ -200,3 +200,15 @@ var BMYAPIBoardRequest = BMYAPIRequest.extend({
 		}
 	}
 });
+
+var BMYAPIBoardListRequest = BMYAPIRequest.extend({
+	init: function(obj) {
+		this.url = 'api/board/list?secstr='+obj.secstr;
+
+		// TODO: 排序参数
+
+		if(typeof(obj.async) != "undefined") {
+			this.async = obj.async;
+		}
+	}
+});
