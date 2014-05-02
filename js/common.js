15efbb4a394b2a123e3c83062b59d2e227af5ed5
IronBlood <aplshenyang@gmail.com>
2014-05-02 20:00:37 +0800

    注销按钮

--- js/common.js
+++ js/common.js
@@ -36,7 +36,7 @@ function load_personal_status(callback) {
 		$.getJSON(url_query_user, function(data) {
 			if(data.errcode == 0) {
 				var total_notify = data.unread_mail + data.unread_notify;
-				$('#bmy-ps-info').html("<div class='btn-group'><button id='nav-user-btn' type='button' class='btn btn-success navbar-btn dropdown-toggle' data-toggle='dropdown'>" + localStorage.userid + "<span class='badge'>" + total_notify + "</span><span class='caret'></span></button><ul class='dropdown-menu' role='menu'><li><a href='#'>站内信<span class='badge pull-right'>" + data.unread_mail + "</span></a></li><li><a href='#'>提醒<span class='badge pull-right'>" + data.unread_notify + "</span></li><li class='divider'></li><li><a href='#'>工具箱</a></li><li class='divider'></li><li><a href='#'>注销</a></li></ul>");
+				$('#bmy-ps-info').html("<div class='btn-group'><button id='nav-user-btn' type='button' class='btn btn-success navbar-btn dropdown-toggle' data-toggle='dropdown'>" + localStorage.userid + "<span class='badge'>" + total_notify + "</span><span class='caret'></span></button><ul class='dropdown-menu' role='menu'><li><a href='#'>站内信<span class='badge pull-right'>" + data.unread_mail + "</span></a></li><li><a href='#'>提醒<span class='badge pull-right'>" + data.unread_notify + "</span></li><li class='divider'></li><li><a href='#'>工具箱</a></li><li class='divider'></li><li><span id='logout-button'>注销</span></li></ul>");
 			} else {
 				$('#bmy-ps-info').html("<span class='btn btn-success navbar-btn' id='login-button'>登录</span>");
 			}
