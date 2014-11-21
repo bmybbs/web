7d26f135ba7964627cf003c3410474c51331be9c
IronBlood <aplshenyang@gmail.com>
2014-11-21 20:51:28 +0800

    修正 bootstrap 的用法

--- Brocfile.js
+++ Brocfile.js
@@ -17,8 +17,9 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import('bower_components/bootstrap/dist/css/bootstrap.css');
 app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', { destDir: 'assets'});
 app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', { destDir: 'fonts'});
 app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', { destDir: 'fonts'});
 app.import('bower_components/moment/min/moment-with-locales.min.js');
