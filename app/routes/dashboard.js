536d7372fdb479466834c2e79a14d3418fb576e8
IronBlood <aplshenyang@gmail.com>
2014-12-03 21:24:13 +0800

    修正判断接口错误码的判断
    实际范围的是数值0，而非字符串'0'

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -18,7 +18,7 @@ export default Ember.Route.extend({
 
 		return Ember.RSVP.hash({
 			top10: bmy_api_top10_req.pull().then(function(data) {
-				if(data.errcode === '0') {
+				if(data.errcode === 0) {
 					var d = data.articlelist;
 					for (var i = 0; i < 3 && i < d.length; i++) {
 						d[i].hot = true;
@@ -27,71 +27,71 @@ export default Ember.Route.extend({
 				}
 			}),
 			announce: bmy_api_announce_req.pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			}),
 			commend: bmy_api_commend_req.pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			}),
 			articlelist_sectop_0: bmy_api_sec_reqs.get('0').pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			}),
 			articlelist_sectop_1: bmy_api_sec_reqs.get('1').pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			}),
 			articlelist_sectop_2: bmy_api_sec_reqs.get('2').pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			}),
 			articlelist_sectop_3: bmy_api_sec_reqs.get('3').pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			}),
 			articlelist_sectop_4: bmy_api_sec_reqs.get('4').pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			}),
 			articlelist_sectop_5: bmy_api_sec_reqs.get('5').pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			}),
 			articlelist_sectop_6: bmy_api_sec_reqs.get('6').pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			}),
 			articlelist_sectop_7: bmy_api_sec_reqs.get('7').pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			}),
 			articlelist_sectop_8: bmy_api_sec_reqs.get('8').pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			}),
 			articlelist_sectop_9: bmy_api_sec_reqs.get('9').pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			}),
 			articlelist_sectop_G: bmy_api_sec_reqs.get('G').pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			}),
 			articlelist_sectop_N: bmy_api_sec_reqs.get('N').pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			}),
 			articlelist_sectop_H: bmy_api_sec_reqs.get('H').pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			}),
 			articlelist_sectop_A: bmy_api_sec_reqs.get('A').pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			}),
 			articlelist_sectop_C: bmy_api_sec_reqs.get('C').pull().then(function(data) {
-				if(data.errcode === '0')
+				if(data.errcode === 0)
 					return data.articlelist;
 			})
 		});
