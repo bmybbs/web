5b94ef0cd0e50fdaeaeab9e3a0473b02aa917ac5
IronBlood <aplshenyang@gmail.com>
2014-12-07 23:23:07 +0800

    使用 ember 的方式控制登录按钮

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,28 +2,7 @@ import Ember from 'ember';
 import BMYAPIUserQueryRequest from '../utils/BMYAPIUserQueryRequest';
 
 var $ = Ember.$;
-
-function load_personal_status(callback) {
-	if(typeof(localStorage.userid) === 'undefined') {
-		$('#bmy-ps-info').html("<span id='login-button' class='btn btn-success navbar-btn'>登录</span>");
-		if(callback && typeof(callback)==="function")
-			callback();
-	}
-	else {
-		var query_req = new BMYAPIUserQueryRequest({ });
-		query_req.pull().then(function(data) {
-			if(data.errcode === 0) {
-				var total_notify = data.unread_mail + data.unread_notify;
-				$('#bmy-ps-info').html("<div class='btn-group'><button id='nav-user-btn' type='button' class='btn btn-success navbar-btn dropdown-toggle' data-toggle='dropdown'>" + localStorage.userid + "<span class='badge'>" + total_notify + "</span><span class='caret'></span></button><ul class='dropdown-menu' role='menu'><li><a href='#'><span class='glyphicon glyphicon-envelope bmy-span-icon'></span>站内信<span class='badge pull-right'>" + data.unread_mail + "</span></a></li><li><a href='#'><span class='glyphicon glyphicon-bullhorn bmy-span-icon'></span>提醒<span class='badge pull-right'>" + data.unread_notify + "</span></li><li class='divider'></li><li><a href='#'><span class='glyphicon glyphicon-cog bmy-span-icon'></span>工具箱</a></li><li class='divider'></li><li><span id='logout-button'><span class='glyphicon glyphicon-off bmy-span-icon'></span>注销</span></li></ul>");
-			} else {
-				$('#bmy-ps-info').html("<span class='btn btn-success navbar-btn' id='login-button'>登录</span>");
-			}
-
-			if(callback && typeof(callback)==="function")
-				callback();
-		});
-	}
-}
+var query_req = new BMYAPIUserQueryRequest({ });
 
 export default Ember.Route.extend({
 	model: function() {
@@ -35,6 +14,29 @@ export default Ember.Route.extend({
 	},
 	setupController: function(controller, model) {
 		controller.set('model', model);
-		setInterval(load_personal_status, 30000);
+
+		query_req.pull().then(function(data) {
+			if(data.errcode === 0) {
+				// 设置用户信息
+				controller.set('total_notify', data.unread_mail + data.unread_notify);
+				controller.set('unread_mail', data.unread_mail);
+				controller.set('unread_notify', data.unread_notify);
+			}
+			controller.set('is_login', (data.errcode === 0));
+			localStorage.is_login = (data.errcode === 0);
+		});
+
+		setInterval(function() {
+			query_req.pull().then(function(data) {
+				if(data.errcode === 0) {
+					// 设置用户信息
+					controller.set('total_notify', data.unread_mail + data.unread_notify);
+					controller.set('unread_mail', data.unread_mail);
+					controller.set('unread_notify', data.unread_notify);
+				}
+				localStorage.is_login = (data.errcode === 0);
+				controller.set('is_login', (data.errcode === 0));
+			});
+		}, 30000);
 	}
 });
