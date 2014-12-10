39db9aaf512530f4c6a55c76bc77f282b599eeec
IronBlood <aplshenyang@gmail.com>
2014-12-10 13:27:27 +0800

    将登出功能迁移到 ember-cli

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import ENV from 'bmy-new-web/config/environment';
 
 var $ = Ember.$;
 
@@ -23,6 +24,34 @@ export default Ember.ObjectController.extend({
 				$('#commandbar').addClass('open');
 			else
 				$('#commandbar').removeClass('open');
+		},
+		logout: function() {
+			var url_logout = ENV.endpoint + 'api/user/logout?userid=' + localStorage.userid + '&sessid=' + localStorage.sessid + '&appkey=' + ENV.appkey;
+			var c = this;
+			$.ajax({
+				type: "GET",
+				url: url_logout,
+				dataType: 'json',
+				success: function(data) {
+					if(data.errcode !== 0)
+						alert(data.errcode);
+					else {
+						localStorage.removeItem("userid");
+						localStorage.removeItem("sessid");
+						localStorage.removeItem("token");
+						localStorage.removeItem("is_rmbme");
+						localStorage.is_login = false;
+						c.controllerFor('application').set('is_login', false);
+						c.controllerFor('index').set('is_login', false);
+
+						c.transitionToRoute('index');
+					}
+				},
+				error: function(xhr, ajaxOptions, thrownError) {
+					alert('服务器好像开小差了，一会儿再试试！');
+					console.log('Login Error: ' + xhr.responseText);
+				}
+			});
 		}
 	}
 });
--- js/common.js
+++ js/common.js
@@ -6,29 +6,3 @@ function get_bmysec_name(sec_id) {
 		return e.id == sec_id;
 	})[0].name;
 }
-
-function bind_logout_button(callback) {
-	$('span#logout-button').click(function() {
-		var url_logout = 'api/user/logout?userid=' + localStorage.userid + '&sessid=' + localStorage.sessid + '&appkey=' + appkey;
-		$.ajax({
-			type: "GET",
-			url: url_logout,
-			dataType: 'json',
-			success: function(data) {
-				if(data.errcode != 0) {
-					alert(data.errcode);
-				} else {
-					localStorage.removeItem("userid");
-					localStorage.removeItem("sessid");
-					localStorage.removeItem("token");
-					localStorage.removeItem("is_rmbme");
-
-					if(callback && typeof(callback)=="function") {
-						callback();
-					}
-				}
-			}
-		});
-	});
-}
-
