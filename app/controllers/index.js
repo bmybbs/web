a8f4eeca398ae0b8f62bf5626c2970fa9ce45ba7
IronBlood <aplshenyang@gmail.com>
2014-12-10 12:12:15 +0800

    将登录功能迁移到 ember-cli 中

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,42 @@
 import Ember from 'ember';
+import ENV from 'bmy-new-web/config/environment';
 
 export default Ember.ObjectController.extend({
-	'is_login': true
+	'is_login': true,
+	actions: {
+		redirect: function() {
+			this.transitionToRoute('dashboard');
+		},
+		login: function() {
+			var $ = Ember.$;
+			var userid   = $('input#username').val();
+			var passwd   = $('input#password').val();
+			var is_rmbme = $('input#chk_remember').is(':checked');
+
+			var url_login = ENV.endpoint + 'api/user/login?userid='+userid+'&passwd='+passwd+'&appkey=' + ENV.appkey;
+
+			$.ajax({
+				type: "GET",
+				url: url_login,
+				dataType: 'json',
+				success: function(data) {
+					if(data.errcode !== 0) {
+						alert(data.errcode);
+					} else {
+						localStorage.userid = data.userid;
+						localStorage.sessid = data.sessid;
+						localStorage.token  = data.token;
+						localStorage.is_rmbme = is_rmbme;
+						localStorage.is_login = true;
+
+						this.transitionToRoute('dashboard');
+					}
+				},
+				error: function(xhr, ajaxOptions, thrownError) {
+					alert('服务器好像开小差了，一会儿再试试！');
+					console.log('Login Error: ' + xhr.responseText);
+				}
+			});
+		}
+	}
 });
--- js/common.js
+++ js/common.js
@@ -7,39 +7,6 @@ function get_bmysec_name(sec_id) {
 	})[0].name;
 }
 
-function bind_login_button(callback) {
-	$('button#btn-login').click(function() {
-		var userid   = $('input#username').val();
-		var passwd   = $('input#password').val();
-		var is_rmbme = $('input#chk_remember').is(':checked');
-
-		var url_login = 'api/user/login?userid='+userid+'&passwd='+passwd+'&appkey=newweb';
-
-		$.ajax({
-			type: "GET",
-			url: url_login,
-			dataType: 'json',
-			success: function(data) {
-				if(data.errcode != 0) {
-					alert(data.errcode);
-				} else {
-					localStorage.userid = data.userid;
-					localStorage.sessid = data.sessid;
-					localStorage.token  = data.token;
-					localStorage.is_rmbme = is_rmbme;
-					if(callback && typeof(callback)=="function") {
-						callback();
-					}
-				}
-			},
-			error: function(xhr, ajaxOptions, thrownError) {
-				alert('oh no!' + xhr.responseText);
-				alert(thrownError);
-			}
-		});
-	});
-}
-
 function bind_logout_button(callback) {
 	$('span#logout-button').click(function() {
 		var url_logout = 'api/user/logout?userid=' + localStorage.userid + '&sessid=' + localStorage.sessid + '&appkey=' + appkey;
