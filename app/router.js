1ec9be5b810bc85bfdb5c33fd1d90781d0c4fc63
IronBlood <aplshenyang@gmail.com>
2014-12-11 21:39:13 +0800

    一些路由的更新

--- app/router.js
+++ app/router.js
@@ -8,18 +8,16 @@ var Router = Ember.Router.extend({
 Router.map(function() {
 	this.resource('index', { path: '/' });
 	this.resource('dashboard');
-	this.resource('section', { path: '/section/:sec_id' });
+	this.route('section', { path: '/section/:section_id' }, function() {
+		this.route('board', { path: '/:board_id' }, function() {
+			this.route('article', { path: '/:article_id' }, function() {
+				this.route('reply');
+				this.route('edit');
+			});
+			this.route('new');
+		});
+	});
 	this.resource('fav', { path: '/fav' });
-
-	this.resource('board', { path: '/section/:sec_id/:board_name' });
-	this.resource('boardThread', { path: '/section/:sec_id/:board_name/thread' });
-	this.resource('boardPage', { path: '/section/:sec_id/:board_name/page/:page_num'});
-
-	this.resource('articleRead', { path: '/section/:sec_id/:board_name/:aid' });
-	this.resource('articlePost', { path: '/section/:sec_id/:board_name/new' });
-	this.resource('articleReply', { path: '/section/:sec_id/:board_name/:aid/reply' });
-
-	this.resource('articleThread', { path: '/section/:sec_id/:board_name/thread/:tid' });
 });
 
 export default Router;
