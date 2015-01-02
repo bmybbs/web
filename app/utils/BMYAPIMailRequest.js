ce4a4543495b8493e666d8de186035c2e455f267
IronBlood <aplshenyang@gmail.com>
2015-01-02 23:45:50 +0800

    邮件正文获取对应的请求实现，支持 HTML/RAW 两种格式

---
+++ app/utils/BMYAPIMailRequest.js
@@ -0,0 +1,13 @@
+import BMYAPIRequest from './BMYAPIRequest';
+import ENV from 'bmy-new-web/config/environment';
+
+export default BMYAPIRequest.extend({
+	init: function(obj) {
+		if((typeof(obj.type) !== "undefined") && (obj.type === "RAW"))
+			this.url = ENV.endpoint + 'api/mail/getRAWContent';
+		else
+			this.url = ENV.endpoint + 'api/mail/getHTMLContent';
+
+		this.url += '&num=' + obj.num;
+	}
+});
