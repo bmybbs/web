275af83415f32ee144d0234a7b90c2ee242886c6
IronBlood <aplshenyang@gmail.com>
2015-01-12 00:03:21 +0800

    信件列表新增发件箱、收件箱切换的参数

---
+++ app/controllers/mails/index.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+	queryParams: ['box_type'],
+	box_type: null,
+
+	actions: {
+
+	}
+});
--- app/routes/mails/index.js
+++ app/routes/mails/index.js
@@ -2,8 +2,15 @@ import Ember from 'ember';
 import BMYAPIMailListRequest from 'bmy-new-web/utils/BMYAPIMailListRequest';
 
 export default Ember.Route.extend({
-	model: function() {
-		var mail_list_req = new BMYAPIMailListRequest({ });
+	queryParams: {
+		box_type: { refreshModel: true }
+	},
+	model: function(params) {
+		var mail_list_req;
+		if(typeof(params.box_type) !== "undefined" && params.box_type === "sent")
+			mail_list_req = new BMYAPIMailListRequest({ box_type:1 });
+		else
+			mail_list_req = new BMYAPIMailListRequest({ });
 
 		return mail_list_req.pull().then(function(data) {
 			return data;
