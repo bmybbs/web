27ad4a9c941a98a5b018e6d6ebe3b78916fa4199
IronBlood <aplshenyang@gmail.com>
2015-02-08 15:17:19 +0800

    收藏夹管理/添加的方法

---
+++ app/utils/BMYAPIBoardFavAddRequest.js
@@ -0,0 +1,11 @@
+import BMYAPIRequest from './BMYAPIRequest';
+import ENV from 'bmy-new-web/config/environment';
+
+export default BMYAPIRequest.extend({
+	init: function(obj) {
+		this.url = ENV.endpoint + 'api/board/fav/list?board=' + obj.board;
+
+		if(typeof(obj.async) !== "undefined")
+			this.async = obj.async;
+	}
+});
