788359c9764719d046b83b9759ab4f20e4b046ac
IronBlood <aplshenyang@gmail.com>
2015-02-26 09:03:20 +0800

    修正接口的路径

--- app/utils/BMYAPIUserRejectsAddRequest.js
+++ app/utils/BMYAPIUserRejectsAddRequest.js
@@ -3,7 +3,7 @@ import ENV from 'bmy-new-web/config/environment';
 
 export default BMYAPIRequest.extend({
 	init: function(obj) {
-		this.url = ENV.endpoint + 'api/user/friends/add?queryid=' + obj.queryid + '&explain=' + obj.explain;
+		this.url = ENV.endpoint + 'api/user/rejects/add?queryid=' + obj.queryid + '&explain=' + obj.explain;
 
 		if(typeof(obj.async) !== "undefined")
 			this.async = obj.async;
