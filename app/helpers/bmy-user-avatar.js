8f31792260933e9a716fadaa831682bdeb8270c6
IronBlood <aplshenyang@gmail.com>
2015-03-08 19:27:34 +0800

    新增两档更小的 avatar 尺寸

--- app/helpers/bmy-user-avatar.js
+++ app/helpers/bmy-user-avatar.js
@@ -2,10 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Handlebars.makeBoundHelper(function(userid, size, option) {
 	var avatar_url = 'images/avatars/';
+	var img_size = 0;
 	if(typeof(userid) === "undefined" || userid.length === 0 || typeof(size) === "undefined" || size.length === 0)
 		return new Ember.Handlebars.SafeString("<img src='images/avatars/48/mystery.png' alt='神秘用户'>");
 
 	switch(size.toLowerCase()) {
+	case 'xxxs': avatar_url += '48/'; img_size = '24'; break;
+	case 'xxs': avatar_url += '48/'; img_size = '36'; break;
 	case 'xs': avatar_url += '48/'; break;
 	case 's': avatar_url += '96/'; break;
 	case 'm': avatar_url += '128/'; break;
@@ -17,5 +20,5 @@ export default Ember.Handlebars.makeBoundHelper(function(userid, size, option) {
 	if(userid === "Anonymous")
 		return new Ember.Handlebars.SafeString("<img src='" + avatar_url + "mystery.png' alt='神秘用户'>");
 
-	return new Ember.Handlebars.SafeString("<img src='" + avatar_url + userid[0].toLowerCase() + ".png' alter='" + userid + "'>");
+	return new Ember.Handlebars.SafeString("<img src='" + avatar_url + userid[0].toLowerCase() + ".png' alter='" + userid + "'" + ((img_size !== 0) ? ("height='" + img_size + "' width='" + img_size + "'") : "" ) +">");
 })
