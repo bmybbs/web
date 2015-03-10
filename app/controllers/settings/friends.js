ca708d1fe3fe9fe0cf1fecfc1d14ff11836f3e72
IronBlood <aplshenyang@gmail.com>
2015-03-10 09:32:12 +0800

    实现：配置页面添加、删除好友的操作

---
+++ app/controllers/settings/friends.js
@@ -0,0 +1,46 @@
+import Ember from 'ember';
+import BMYAPIUserFriendsListRequest from '../../utils/BMYAPIUserFriendsListRequest';
+import BMYAPIUserFriendsAddRequest from '../../utils/BMYAPIUserFriendsAddRequest';
+import BMYAPIUserFriendsDelRequest from '../../utils/BMYAPIUserFriendsDelRequest';
+
+export default Ember.ObjectController.extend({
+	friendId: '',
+	friendExp: '',
+	actions: {
+		addFriend: function() {
+			var self = this;
+			var ufa_req = new BMYAPIUserFriendsAddRequest({ queryid: this.get('friendId'), explain: this.get('friendExp') });
+			ufa_req.pull().then(function(data) {
+				if(data.errcode === 0) {
+					var uflr = new BMYAPIUserFriendsListRequest({ });
+					uflr.pull().then(function(data) {
+						self.set('model', data);
+					});
+				} else if(data.errcode === 100000) {
+					alert('没有找到相关用户...您再瞅瞅？');
+				} else if(data.errcode === 100009) {
+					alert('已达到最大记录数，无法添加。');
+				} else if(data.errcode === 100010) {
+					alert('记录已存在，不用再添加啦！');
+				} else {
+					alert('系统出错啦 :(');
+				}
+			});
+		},
+		delFriend: function(userid) {
+			var self = this;
+			var ufd_req = new BMYAPIUserFriendsDelRequest({ queryid: userid });
+			ufd_req.pull().then(function(data) {
+				if(data.errcode === 0) {
+					Ember.$('tr#' + userid).remove();
+				} else if(data.errcode === 100000) {
+					alert('没有找到相关用户...您再瞅瞅？');
+				} else if(data.errcode === 100011) {
+					alert('记录不存在，无法删除哦~');
+				} else {
+					alert('系统出错啦 :(');
+				}
+			});
+		}
+	}
+});
