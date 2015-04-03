cc1e4c694f128e89ba0dda37228f94d57bfa8354
IronBlood <aplshenyang@gmail.com>
2015-04-03 21:47:43 +0800

    版面默认使用主题方式浏览，同时到序查看

--- app/routes/board.js
+++ app/routes/board.js
@@ -13,7 +13,7 @@ export default Ember.Route.extend({
 		var al = new BMYAPIArticleListRequest({
 			"type": "board",
 			"board": params.board_id,
-			"btype": (typeof(params.readtype) !== "undefined" && params.readtype === "thread") ? "t" : "0",
+			"btype": (typeof(params.readtype) === "undefined" || params.readtype !== "thread") ? "t" : "0",
 			"page": (typeof(params.page) !== "undefined" && params.page !== null) ? params.page : 1
 		});
 		var bt = new BMYAPIArticleListRequest({ "type": "boardtop", "board": params.board_id });
@@ -26,7 +26,9 @@ export default Ember.Route.extend({
 				for(var i=0; i<data.articlelist.length; ++i)
 					data.articlelist[i].secstr = params.section_id;
 
-				return data.articlelist;
+				return data.articlelist.sort(function(a, b) {
+					return b.aid - a.aid;
+				});
 			}),
 			boardtop: bt.pull().then(function(data) {
 				if(data.errcode === 0)
